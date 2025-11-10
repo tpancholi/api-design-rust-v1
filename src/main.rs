56290e79dcd8dc029e93dca7a9bebc3e6f881024
Tejas Pancholi <tejas.pancholi@deepshieldai.com>
2025-11-10 21:39:16 +0530

    added unit test case

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use api_design_rust_v1::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to start server");
+    run(listener)?.await
 }
